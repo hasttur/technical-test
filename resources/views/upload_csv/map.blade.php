733314472779e2b002ee339b36246ae25960e919
Nilson Molina <hasttur@outlook.com>
2024-06-07 16:50:18 -0500

    finaliza prueba tecnica

--- resources/views/upload_csv/map.blade.php
+++ resources/views/upload_csv/map.blade.php
@@ -16,7 +16,7 @@
     let data = @json($data);
 
     var map = L.map('map').setView([4.624335, -74.063644], 10);
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { // Define la capa de base del mapa
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 
